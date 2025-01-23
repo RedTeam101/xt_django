dd751d52efeb30c6b8009e390d2422eeed5d1d91
tho-hk <taine.ho@redteam101.com>
2025-01-23 12:35:03 +0800

    completed simple template variable demo

--- tem_app/urls.py
+++ tem_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-#from . import views             # 同目录下的 views.py
+from tem_app.views import tem_var             # 同目录下的 views.py
+
 
 urlpatterns = [
-    path('', views.index, name='index'),    # 弯弯的月亮 api
-    path('sun/', views.sun, name='sun'),    # 灿烂的太阳 api
-    path('star/', views.star, name='star'), # 闪烁的星星 api
+    path("var/<name>/", tem_var),
 ]
--- tem_app/views.py
+++ tem_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def tem_var(request, name):
-    return render(request, "")
+    return render(request, "show_var.html", {"name": name})
+    # it will display the text after api endpoint as header
+    # e.g. http://127.0.0.1:8000/tem_app/var/12345/
+    # will show: "12345" as header
